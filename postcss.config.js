7ff81cb5ef75ac00071c89e432a04e914be5dc6b
hubcarl <hubcarl@126.com>
2017-04-24 21:24:33 +0800

    fix:autoprifix

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,10 @@
  * see: https://github.com/akveo/ng2-admin/issues/604#issuecomment-271974780
  */
 
-module.exports = {};
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: ['last 3 versions', 'Firefox ESR', '> 10%', 'ie >= 8'],
+    }),
+  ],
+};
