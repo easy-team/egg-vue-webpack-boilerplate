02bce6a16cf0d64341f4131562e303f72e90ffe2
hubcarl <hubcarl@126.com>
2017-05-02 22:46:53 +0800

    fix:npm start env

--- build/easy/build.js
+++ build/easy/build.js
@@ -1,7 +1,6 @@
 const EasyWebpack = require('easywebpack');
 
 const clientConfig = require('./client');
-console.log(clientConfig);
 const serverConfig = require('./server');
 //const tmpServerConfig = Object.assign({},serverConfig);
 //delete tmpServerConfig.externals;
--- build/easy/server.js
+++ build/easy/server.js
@@ -1,5 +1,8 @@
+const path = require('path');
+const fs = require('fs');
 const webpackConfig = require('../../config/webpackConfig');
 const BaseBuilder = require('./base');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 class ServerBuilder extends BaseBuilder {
   constructor(config) {
     super(config, { isServer: true });
@@ -15,6 +18,23 @@ class ServerBuilder extends BaseBuilder {
       default:
         break;
     }
+    const baseDir = this.config.baseDir;
+    this.setLoader({
+      test: /layout\/standard\/index\.js$/,
+      loader: StringReplacePlugin.replace({
+        replacements: [
+          {
+            pattern: /["|'](.*\.html)\?inline["|']/gi,
+            replacement(match, p1, offset, string) {
+              const filePath = path.join(baseDir, p1);
+              const content = fs.readFileSync(filePath, 'utf8').replace(/'/g, '"');
+              return `\`${content}\``;
+            }
+          }
+        ]
+      })
+    });
+    this.setPlugin(new StringReplacePlugin());
   }
 }
 
