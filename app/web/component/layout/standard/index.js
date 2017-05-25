6978dc48220c7bbc40c700301075f39fd9f751d5
hubcarl <hubcarl@126.com>
2017-05-25 23:17:25 +0800

    fix:support window

--- app/web/component/layout/standard/index.js
+++ app/web/component/layout/standard/index.js
@@ -3,9 +3,22 @@ import MainLayout from './main';
 
 const content = '<div id="app"><MainLayout><div slot="main"><slot></slot></div></MainLayout></div>';
 
-let template = 'app/web/view/layout.html?inline';
-
-template = template.replace(/[<|\\x3c]!--vue-ssr-outlet--[>|\\x3e]/, content);
+const template = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>{{title}}</title>
+  <meta name="keywords" :content="keywords">
+  <meta name="description" :content="description">
+  <meta http-equiv="content-type" content="text/html;charset=utf-8">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-default/index.css">
+</head>
+<body :class="baseClass">
+  <div id="app">${content}</div>
+</body>
+</html>`;
 
 export default {
   name: 'Layout',
--- build/server/base.js
+++ build/server/base.js
@@ -1,28 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 const EggWebpackVue = require('egg-webpack-vue');
-const StringReplacePlugin = require('string-replace-webpack-plugin');
 const base = require('../base');
 class ServerBuilder extends EggWebpackVue.WebpackServerBuilder {
   constructor() {
     super(base.config);
     this.setOption(base.getOption(base.config));
-    this.setLoader({
-      test: /layout\/standard\/index\.js$/,
-      loader: StringReplacePlugin.replace({
-        replacements: [
-          {
-            pattern: /["|'](.*\.html)\?inline["|']/gi,
-            replacement(match, p1, offset, string) {
-              const filePath = path.join(base.config.baseDir, p1);
-              const content = fs.readFileSync(filePath, 'utf8').replace(/'/g, '"').replace(/[\r\n]/g, '');
-              return "'" + content + "'";
-            }
-          }
-        ]
-      })
-    }, true);
-    this.setPlugin(new StringReplacePlugin(), true);
   }
 }
 
