58aea348c63ce234d617c346f2f913a6e40350e1
hubcarl <hubcarl@126.com>
2017-05-25 21:27:25 -0700

    test:win test

---
+++ test.js
@@ -0,0 +1,28 @@
+const value = 'static/js/\\element\\element.js';
+const key = '\\element\\element.js';
+const nvalue = value.replace(/\\/g, '/').replace(/\/\//g, '/');
+const nkey = key.replace(/^\\/g, '').replace(/\\/g, '/');
+console.log(nkey, nvalue);
+
+const manifest = {
+  "\\about\\about.js": "static/js/\\about\\about.js",
+  "\\app\\app.js": "static/js/\\app\\app.js",
+  "\\element\\element.js": "static/js/\\element\\element.js",
+  "\\front\\front.js": "static/js/\\front\\front.js",
+  "\\index\\index.js": "static/js/\\index\\index.js",
+  "\\router\\router.js": "static/js/\\router\\router.js",
+  "\\test\\test.js": "static/js/\\test\\test.js",
+  "static\\img\\loading.gif": "static/img/loading.gif",
+  "static\\img\\logo.png": "static/img/logo.png",
+  "vendor.js": "static/js/vendor.js"
+};
+
+
+const normalizeManifest = {};
+Object.keys(manifest).forEach(key => {
+   const normalizekey = key.replace(/^\\/g, '').replace(/\\/g, '/');
+   const normalizeValue = manifest[key].replace(/\\/g, '/').replace(/\/\//g, '/');
+   normalizeManifest[normalizekey] = normalizeValue;
+});
+console.log(normalizeManifest);
+return normalizeManifest;
