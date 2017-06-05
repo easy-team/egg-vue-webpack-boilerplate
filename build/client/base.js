15e00c63d6363904324f9b429db5ec4b5f7b00ff
hubcarl <hubcarl@126.com>
2017-06-05 17:15:34 +0800

    test:SSRPlugin Test

--- build/client/base.js
+++ build/client/base.js
@@ -1,9 +1,12 @@
 const EggWebpackVue = require('egg-webpack-vue');
 const base = require('../base');
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
+
 class ClientBuilder extends EggWebpackVue.WebpackClientBuilder {
   constructor() {
     super(base.config);
     this.setOption(base.getOption(base.config));
+    this.addPlugin(VueSSRClientPlugin);
   }
 }
 
--- build/server/base.js
+++ build/server/base.js
@@ -1,9 +1,12 @@
 const EggWebpackVue = require('egg-webpack-vue');
 const base = require('../base');
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
+
 class ServerBuilder extends EggWebpackVue.WebpackServerBuilder {
   constructor() {
     super(base.config);
     this.setOption(base.getOption(base.config));
+    this.addPlugin(VueSSRServerPlugin);
   }
 }
 
