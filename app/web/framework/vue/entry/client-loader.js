30dee76e50b6ba23d88865d3139177c7c0014ff4
hubcarl <hubcarl@126.com>
2017-08-29 15:19:35 +0800

    fix:window load path error

--- app/web/framework/vue/entry/client-loader.js
+++ app/web/framework/vue/entry/client-loader.js
@@ -2,7 +2,7 @@
 module.exports = function(source) {
   return `
     import Client from 'client';
-    import Page from '${this.resourcePath}';
+    import Page from '${this.resourcePath.replace(/\\/g, '\\\\')}';
     export default Client.render({ ...Page });
   `;
 };
--- app/web/framework/vue/entry/server-loader.js
+++ app/web/framework/vue/entry/server-loader.js
@@ -2,7 +2,7 @@
 module.exports = function(source) {
   return `
     import Server from 'server';
-    import Page from '${this.resourcePath}';
+    import Page from '${this.resourcePath.replace(/\\/g, '\\\\')}';
     export default Server.render({ ...Page });
   `;
 };
