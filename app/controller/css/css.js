b9f6f6059061a1f3b27e6f27fcb0f298681af60b
hubcarl <hubcarl@126.com>
2017-09-08 14:19:33 +0800

    feat:eslint

--- app/controller/css/css.js
+++ app/controller/css/css.js
@@ -5,3 +5,8 @@ exports.less = function* () {
 exports.sass = function* () {
   yield this.render('sass/sass.js', { message: 'vue server side render, support sass' });
 };
+
+
+exports.module = function* () {
+  yield this.render('css/module/module.js', { message: 'vue server side render, support css module' });
+};
