1734520c2718c4198269837f8ecec96dac065691
caoli <cl98914@alibaba-inc.com>
2018-03-08 13:50:10 +0800

    fix: remove webpack4 compatible

--- script/postinstall.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const path = require('path');
-const fs = require('fs');
-const source = path.join(process.cwd(), 'script/webpack4/webpack/lib/node/NodeMainTemplatePlugin.js');
-const target = path.join(process.cwd(), 'node_modules/webpack/lib/node/NodeMainTemplatePlugin.js');
-fs.createReadStream(source).pipe(fs.createWriteStream(target));
