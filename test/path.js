8caa21141db8cf5dcb481e9e34751bb560aad266
caoli <cl98914@alibaba-inc.com>
2018-03-13 11:57:48 +0800

    test: path test

--- test/path.js
+++ test/path.js
@@ -4,3 +4,8 @@ const prefix = 'static/test';
 const url = 'js/index.js';
 console.log('--path.join--', path.join(prefix, url));
 console.log('--path.posix.join--', path.posix.join(prefix, url));
+
+// windows print
+
+// --path.join-- static\test\js\index.js
+// --path.posix.join-- static/test/js/index.js
