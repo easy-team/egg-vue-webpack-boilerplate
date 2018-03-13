dde9bb1ab124f466c7943a9ff9e9904a3726f101
caoli <cl98914@alibaba-inc.com>
2018-03-13 11:51:09 +0800

    test: path test

---
+++ test/path.js
@@ -0,0 +1,6 @@
+'use strict';
+const path = require('path');
+const prefix = 'static/test';
+const url = 'js/index.js';
+console.log('--path.join--', path.join(prefix, url));
+console.log('--path.posix.join--', path.posix.join(prefix, url));
