c1df7f18d4d9429e26ce566dac60fa6f4c85ae0a
caoli <cl98914@alibaba-inc.com>
2018-09-29 18:40:41 +0800

    fix: egg worker reload for config/manifest.json update

--- config/config.local.js
+++ config/config.local.js
@@ -13,7 +13,7 @@ module.exports = app => {
 
   exports.development = {
     watchDirs: [], // 指定监视的目录（包括子目录），当目录下的文件变化的时候自动重载应用，路径从项目根目录开始写
-    ignoreDirs: ['app/web', 'public'] // 指定过滤的目录（包括子目录）
+    ignoreDirs: ['app/web', 'public', 'config/manifest.json'] // 指定过滤的目录（包括子目录）
   };
 
   exports.logview = {
