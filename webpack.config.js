3bf54bd8c31cf980e9b9534bb3ac3f2ec98f8046
caoli <cl98914@alibaba-inc.com>
2018-10-23 14:25:58 +0800

    fix: webpack.config.js merge vux

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 // target: web 表示只获取前端构建 Webpack 配置
 // refer: https://www.yuque.com/easy-team/easywebpack/native
-const easywebpack = require('easywebpack-vue');
 const vuxLoader = require('vux-loader');
-const baseWebpackConfig = easywebpack.getWebpackConfig({
-  target: 'vue', // target: web 表示只获取前端构建 Webpack 配置
-});
 // 拿到基础配置, 可以进行二次加工
-module.exports = vuxLoader.merge(baseWebpackConfig, {
+module.exports = vuxLoader.merge({}, {
   plugins: ['vux-ui']
 });
