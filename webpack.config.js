f7ffc334183185b75f060cf90bcbaa65f3099405
HiuYanChong <sysu_se_zxx.@163.com>
2018-11-06 11:14:22 +0800

    feat: try to split common css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@
 // 零配置简化, 详情：https://www.yuque.com/easy-team/easywebpack/v4
 module.exports = {
   plugins: {
-    imagemini: false
+    imagemini: false,
+    extract: true,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commonCss: {
+          test: /\.s?css$/,
+          minChunks: 4,
+          name: 'styles',
+          chunks: 'all'
+        }
+      }
+    }
   }
 };
