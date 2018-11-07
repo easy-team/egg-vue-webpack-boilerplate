cdbf1e701c256ce7856139be5b243a2847ea4d89
HiuYanChong <sysu_se_zxx.@163.com>
2018-11-07 10:31:45 +0800

    feat: 使用 vue-content-loader 制作骨架屏

--- app/web/page/category/category.vue
+++ app/web/page/category/category.vue
@@ -1,34 +1,35 @@
 <template>
   <layout description='vue server side render' keywords='egg, vue, webpack, server side render'>
-     <div class='container'>
+    <content-loader v-if="loading">
+      <rect x="40" y="0" rx="3" ry="3" width="250" height="20" />
+    </content-loader>
+     <div class='container' v-else>
         <h2>{{ message }}</h2>
      </div>
   </layout>
 </template>
 <style>
-  @import 'category.css';
+@import "category.css";
 </style>
 <script type='babel'>
-  
-  export default {
-    components: {
-
-    },
-    data(){
-      return {
-      }
-    },
-    computed: {
-      
-    },
-    methods: {
-      
-    },
-    mounted() {
-      if('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/public/sw-category.js');
-      }
+import { ContentLoader } from "vue-content-loader";
+export default {
+  components: {
+    ContentLoader
+  },
+  data() {
+    return {
+      loading: true
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted() {
+    this.loading = false;
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/public/sw-category.js");
     }
   }
+};
 </script>
 
