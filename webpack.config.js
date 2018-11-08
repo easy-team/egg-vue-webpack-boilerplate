1757274883fd3d036357938dfbfb12d3aa03907f
庄晓欣 <xiaoxin.zxx@alibaba-inc.com>
2018-11-08 09:38:41 +0800

    feat: 尝试接入 page-skeleton-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { SkeletonPlugin } = require('page-skeleton-webpack-plugin');
 // Document：https://www.yuque.com/easy-team/easywebpack 和 https://www.yuque.com/easy-team/egg-vue 
 module.exports = {
   plugins: {
-    imagemini: false
-  }
+    imagemini: false,
+    html: {
+      name: new HtmlWebpackPlugin({
+        path: __dirname + './dist',
+      })
+    },
+    skeleton: {
+      name: new SkeletonPlugin({
+        pathname: path.resolve(__dirname, './dist/shell'), // the path to store shell file
+        staticDir: path.resolve(__dirname, './dist'), // the same as the `output.path`
+        routes: ['/'],
+        port: '8080',
+      })
+    }
+  },
 };
