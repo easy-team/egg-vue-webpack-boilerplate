8ef6aa8b806b8ac7e76c5b04efdefa3cd4d3e189
sky <hubcarl@126.com>
2019-07-27 12:45:21 +0800

    fix: server side render hydrate not effective

--- app/web/component/layout/admin/index.js
+++ app/web/component/layout/admin/index.js
@@ -3,5 +3,5 @@ import MainLayout from './main.vue';
 import '../../../asset/css/global.css';
 import './index.css';
 import createLayout from '../layout';
-const tpl = '<div class="admin" id="app"><MainLayout><div slot="main"><slot></slot></div></MainLayout></div>';
+const tpl = '<div class="admin" id="app" data-server-rendered="true"><MainLayout><div slot="main"><slot></slot></div></MainLayout></div>';
 export default createLayout('Layout', { MainLayout }, tpl);
