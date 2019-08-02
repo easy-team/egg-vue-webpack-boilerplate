c95aecdb21c52304a7581a04251151df010f9a9a
sky <hubcarl@126.com>
2019-08-02 19:26:28 +0800

    feat: load component on demand

--- app/web/page/category/category.vue
+++ app/web/page/category/category.vue
@@ -2,6 +2,7 @@
   <layout description='vue server side render' keywords='egg, vue, webpack, server side render'>
      <div class='container'>
         <h2>{{ message }}</h2>
+        <el-button type="primary">element-ui</el-button>
      </div>
   </layout>
 </template>
@@ -9,7 +10,9 @@
   @import 'category.css';
 </style>
 <script type='babel'>
-  
+  import Vue from 'vue';
+  import { Button, Select } from 'element-ui';
+  Vue.use(Button);
   export default {
     components: {
 
